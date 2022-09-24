1439f57f4ee98b4f391ad57decadac4051a9072c
Milles Lee <miles.gr.lee@gmail.com>
2022-09-24 15:49:25 -0500

    Working on Camera Controller, added variables and created test scene

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Transform _follow;
+    [SerializeField] private Transform _lookAt;
+    [Header("Axis Control")]
+    private Vector3 _currentPosition;
+    [Header("Orbits (X = Height, Y = Radius)")]
+    [SerializeField] private Vector2 _topRig;
+    [SerializeField] private Vector2 _middleRig;
+    [SerializeField] private Vector2 _bottomRig;
+
     void Start()
     {
-        
+        _currentPosition = new Vector3(0.0f, _middleRig.x, _middleRig.y);
     }
 
-    // Update is called once per frame
     void Update()
     {
         
